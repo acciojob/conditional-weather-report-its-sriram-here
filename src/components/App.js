be7f19945c549a1cbf07f5d86ee32058b80f6d24
SRIRAM <srirammurugesan1807@gmail.com>
2025-08-29 11:51:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay";
+
 
 const App = () => {
   const [weatherData, setWeatherData] = useState({
